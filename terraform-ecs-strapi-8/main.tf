ef3b345cc2879bdf79cbe09d30e6f7f5fa645f15
Adarsh Mane <adarshmane007@gmail.com>
2025-10-03 13:23:39 +0530

    task-10

--- terraform-ecs-strapi-8/main.tf
+++ terraform-ecs-strapi-8/main.tf
@@ -138,6 +138,8 @@ resource "aws_ecs_service" "strapi_service_am_10" {
   task_definition = aws_ecs_task_definition.strapi_task_am_10.arn
   desired_count   = 1
 
+  force_new_deployment = true
+
   capacity_provider_strategy {
     capacity_provider = "FARGATE"
     weight            = 1
