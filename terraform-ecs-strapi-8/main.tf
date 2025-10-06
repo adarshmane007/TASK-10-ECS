e31b4e5e2275d93b1c86250e7368cccf98dd393c
Adarsh Mane <adarshmane007@gmail.com>
2025-10-06 10:43:07 +0530

    task-10

--- terraform-ecs-strapi-8/main.tf
+++ terraform-ecs-strapi-8/main.tf
@@ -200,6 +200,22 @@ resource "aws_codedeploy_deployment_group" "strapi_group" {
     }
   }
 
+  blue_green_deployment_config {
+    terminate_blue_instances_on_deployment_success {
+      action = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+      wait_time_in_minutes = 0
+    }
+
+    green_fleet_provisioning_option {
+      action = "DISCOVER_EXISTING"
+    }
+  }
+
   auto_rollback_configuration {
     enabled = true
     events  = ["DEPLOYMENT_FAILURE"]
