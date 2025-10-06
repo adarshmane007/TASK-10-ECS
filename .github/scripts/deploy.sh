bb107ff47f3fea703816cca53abe14e99b11804c
Adarsh Mane <adarshmane007@gmail.com>
2025-10-06 13:56:19 +0530

    Fix: use cli-input-json for CodeDeploy

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+##!/bin/bash
 
 set -e
 
 # Generate AppSpec content
-echo 'version: 1
+cat <<EOF > deployment.yml
+version: 1
 Resources:
   - TargetService:
       Type: AWS::ECS::Service
@@ -12,15 +13,28 @@ Resources:
         LoadBalancerInfo:
           ContainerName: strapi
           ContainerPort: 1337
-        PlatformVersion: LATEST' > deployment.yml
+        PlatformVersion: LATEST
+EOF
 
-# Flatten and escape AppSpec content
+# Flatten content for JSON
 CONTENT=$(cat deployment.yml | sed 's/"/\\"/g' | tr -d '\n')
 
+# Create full JSON input
+cat <<EOF > deploy.json
+{
+  "applicationName": "strapi-codedeploy-app",
+  "deploymentGroupName": "strapi-bluegreen-group",
+  "deploymentConfigName": "CodeDeployDefault.ECSCanary10Percent5Minutes",
+  "description": "Blue/Green deployment triggered by GitHub Actions",
+  "revision": {
+    "type": "AppSpecContent",
+    "appSpecContent": {
+      "content": "$CONTENT",
+      "sha256": ""
+    }
+  }
+}
+EOF
+
 # Trigger CodeDeploy deployment
-aws deploy create-deployment \
-  --application-name strapi-codedeploy-app \
-  --deployment-group-name strapi-bluegreen-group \
-  --deployment-config-name CodeDeployDefault.ECSCanary10Percent5Minutes \
-  --description "Blue/Green deployment triggered by GitHub Actions" \
-  --revision type=AppSpecContent,appSpecContent="{\"content\":\"$CONTENT\",\"sha256\":\"\"}"
+aws deploy create-deployment --cli-input-json file://deploy.json
